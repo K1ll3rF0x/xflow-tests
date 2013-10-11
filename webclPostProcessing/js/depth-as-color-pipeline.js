b4d0304072a566ad8cc739a3ef1ac96130efa9e6
Toni Dahl <k1ll3rf0x@gmail.com>
2013-10-11 18:12:53 +0300

    Created a custom rendering pipeline that can be utilised for post processing a rendered scene in WebCL context. WebCL kernel has not been added yet. (WIP)

--- webclPostProcessing/js/depth-as-color-pipeline.js
+++ webclPostProcessing/js/depth-as-color-pipeline.js
@@ -1,18 +1,19 @@
 (function () {
-    var depthPipeline, forwardPipeline, currentPipeline, renderI, injectDepthPipeline, swapPipelines;
+    var PostProcessingPipeline, forwardPipeline, currentPipeline, renderI, injectDepthPipeline, swapPipelines;
 
     injectDepthPipeline = function () {
         var xml3ds = document.getElementsByTagName("xml3d");
 
         if (xml3ds[0]) {
             renderI = xml3ds[0].getRenderInterface();
+
             //The normal forward rendering pipeline is always available initially
             //It's also available as a render pass under the constructor XML3D.webgl.ForwardRenderPass(context),
             forwardPipeline = renderI.getRenderPipeline();
 
-            depthPipeline = new XML3D.webgl.DepthRenderPipeline(renderI.context);
-            depthPipeline.init();
-            renderI.setRenderPipeline(depthPipeline);
+            PostProcessingPipeline = new XML3D.webgl.PostProcessingPipeline(renderI.context);
+            PostProcessingPipeline.init();
+            renderI.setRenderPipeline(PostProcessingPipeline);
             currentPipeline = "depth";
         }
     };
@@ -23,7 +24,7 @@
                 renderI.setRenderPipeline(forwardPipeline);
                 currentPipeline = "forward";
             } else {
-                renderI.setRenderPipeline(depthPipeline);
+                renderI.setRenderPipeline(PostProcessingPipeline);
                 currentPipeline = "depth";
             }
         }
@@ -34,16 +35,17 @@
 
     (function (webgl) {
 
-        var DepthRenderPipeline = function (context) {
+        var PostProcessingPipeline = function (context) {
             webgl.RenderPipeline.call(this, context);
             this.createRenderPasses();
         };
 
-        XML3D.createClass(DepthRenderPipeline, webgl.RenderPipeline);
+        XML3D.createClass(PostProcessingPipeline, webgl.RenderPipeline);
 
-        XML3D.extend(DepthRenderPipeline.prototype, {
+        XML3D.extend(PostProcessingPipeline.prototype, {
             init: function () {
                 var context = this.context;
+
                 //Also available: webgl.GLScaledRenderTarget
                 var backBuffer = new webgl.GLRenderTarget(context, {
                     width: context.canvasTarget.width,
@@ -59,6 +61,7 @@
 
                 //The screen is always available under context.canvastarget
                 this.addRenderTarget("screen", context.canvastarget);
+
                 //Remember to initialize each render pass
                 this.renderPasses.forEach(function (pass) {
                     if (pass.init) {
@@ -70,76 +73,66 @@
             createRenderPasses: function () {
                 //This is where the render process is defined as a series of render passes. They will be executed in the
                 //order that they are added. XML3D.webgl.ForwardRenderPass may be used to draw all visible objects to the given target
-                var depthPass = new XML3D.webgl.DepthPass(this, "backBufferOne");
-                this.addRenderPass(depthPass);
 
-                //Blitpass uses backBufferOne as input and simply draws it to the screen
-                var blitPass = new XML3D.webgl.BlitPass(this, "screen", {inputs: { inputTexture: "backBufferOne" }});
-                this.addRenderPass(blitPass);
+                var forwardPass1 = new XML3D.webgl.ForwardRenderPass(this, "backBufferOne"),
+                    webCLPass = new XML3D.webgl.WebCLPass(this, "backBufferOne", {inputs: { inputTexture: "backBufferOne" }}),
+                    BlitPass = new XML3D.webgl.BlitPass(this, "screen", {inputs: { inputTexture: "backBufferOne" }});
+
+                this.addRenderPass(forwardPass1);
+                this.addRenderPass(webCLPass);
+                this.addRenderPass(BlitPass);
             }
         });
 
-        webgl.DepthRenderPipeline = DepthRenderPipeline;
+        webgl.PostProcessingPipeline = PostProcessingPipeline;
 
     })(XML3D.webgl);
 
 
     (function (webgl) {
 
-        var DepthPass = function (pipeline, output, opt) {
+        var WebCLPass = function (pipeline, output, opt) {
             webgl.BaseRenderPass.call(this, pipeline, output, opt);
         };
 
-        XML3D.createClass(DepthPass, webgl.BaseRenderPass, {
+        XML3D.createClass(WebCLPass, webgl.BaseRenderPass, {
             init: function (context) {
-                var shader = context.programFactory.getProgramByName("depthAsColor");
-                this.pipeline.addShader("depthShader", shader);
+                this.debugCanvas = document.getElementById("debug");
+                this.debugCtx = this.debugCanvas.getContext("2d");
+                this.textureBuffer = new Uint8Array(context.canvasTarget.width * context.canvasTarget.height * 4);
+                this.gl = this.pipeline.context.gl;
             },
 
-            render: (function () {
-                var c_projMat_tmp = XML3D.math.mat4.create();
-                var tmpModelViewProjection = XML3D.math.mat4.create();
+            render: function (scene) {
+                var gl = this.gl, sourceTex, pixelData, imageData;
 
-                return function (scene) {
-                    var gl = this.pipeline.context.gl;
-                    var objects = scene.ready;
-                    var target = this.pipeline.getRenderTarget(this.output);
-                    target.bind();
+                //Request the framebuffer from the render pipeline, using its name (in this case 'backBufferOne')
+                sourceTex = this.pipeline.getRenderTarget(this.inputs.inputTexture);
 
-                    gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-                    gl.viewport(0, 0, target.getWidth(), target.getHeight());
-                    gl.enable(gl.DEPTH_TEST);
+                sourceTex.bind();
+                gl.readPixels(0, 0, sourceTex.height, sourceTex.width, gl.RGBA, gl.UNSIGNED_BYTE, this.textureBuffer);
+                sourceTex.unbind();
 
-                    scene.getActiveView().getProjectionMatrix(c_projMat_tmp, target.width / target.height);
-                    scene.updateReadyObjectsFromActiveView(target.getWidth() / target.getHeight());
+                // Debug code ---
+                pixelData = new Uint8ClampedArray(this.textureBuffer);
+                imageData = this.debugCtx.createImageData(sourceTex.height, sourceTex.width);
+                imageData.data.set(pixelData);
+                this.debugCtx.putImageData(imageData, 0, 0);
+                
+                // --- Debug end
 
-                    var parameters = {};
-                    var program = this.pipeline.getShader("depthShader");
-                    program.bind();
+                // TODO: Do something cool with WebCL here by modifying the texturebuffer in WebCL context.
 
-                    for (var i = scene.firstOpaqueIndex, n = objects.length; i < n; i++) {
-                        var obj = objects[i];
-                        if (!obj.isVisible())
-                            continue;
+                // We are giving input directly to output for now...
+                this.output = sourceTex;
 
-                        var mesh = obj.mesh;
-                        XML3D.debug.assert(mesh, "We need a mesh at this point.");
-
-                        obj.getModelViewProjectionMatrix(tmpModelViewProjection);
-                        parameters["modelViewProjectionMatrix"] = tmpModelViewProjection;
-                        program.setUniformVariables(parameters);
-                        mesh.draw(program);
-                    }
-                    program.unbind();
-                    target.unbind();
-                }
-            })()
-
+            }
         });
 
-        webgl.DepthPass = DepthPass;
+        webgl.WebCLPass = WebCLPass;
 
     }(XML3D.webgl));
+
 
     (function (webgl) {
 
@@ -150,7 +143,7 @@
 
         XML3D.createClass(BlitPass, webgl.BaseRenderPass, {
             init: function (context) {
-                var shader = context.programFactory.getProgramByName("colorFromDepth");
+                var shader = context.programFactory.getProgramByName("drawTexture");
                 this.pipeline.addShader("blitShader", shader);
                 this.screenQuad = new XML3D.webgl.FullscreenQuad(context);
                 this.canvasSize = new Float32Array([context.canvasTarget.width, context.canvasTarget.height]);
@@ -160,7 +153,7 @@
                 var gl = this.pipeline.context.gl;
                 var target = this.pipeline.getRenderTarget(this.output);
                 target.bind();
-                gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+                gl.clear(gl.DEPTH_BUFFER_BIT || gl.COLOR_BUFFER_BIT);
 
                 var program = this.pipeline.getShader("blitShader");
                 program.bind();
@@ -179,34 +172,7 @@
 
     }(XML3D.webgl));
 
-    XML3D.shaders.register("depthAsColor", {
-
-        vertex: [
-            "attribute vec3 position;",
-            "uniform mat4 modelViewProjectionMatrix;",
-
-            "void main(void) {",
-            "   gl_Position = modelViewProjectionMatrix * vec4(position, 1.0);",
-            "}"
-        ].join("\n"),
-
-        fragment: [
-            "const vec4 bitShift = vec4(16777216.0, 65536.0, 256.0, 1.0);",
-            "const vec4 bitMask = vec4(0.0, 1.0/256.0, 1.0/256.0, 1.0/256.0);",
-
-            "void main(void) {",
-            "    float depth = gl_FragCoord.z;",
-            "    vec4 encoded = fract(depth * bitShift);",
-            "    encoded = encoded - encoded.xxyz * bitMask;",
-            "    gl_FragColor = encoded;",
-            "}"
-        ].join("\n"),
-
-        uniforms: {
-        }
-    });
-
-    XML3D.shaders.register("colorFromDepth", {
+    XML3D.shaders.register("drawTexture", {
 
         vertex: [
             "attribute vec3 position;",
@@ -220,12 +186,9 @@
             "uniform sampler2D inputTexture;",
             "uniform vec2 canvasSize;",
 
-            "const vec4 bitShift = vec4(1.0 / (256.0 * 256.0 * 256.0), 1.0 / (256.0 * 256.0), 1.0 / 256.0, 1.0);",
-
             "void main(void) {",
-            "    vec2 texcoord = (gl_FragCoord.xy / canvasSize.xy);",
-            "    float depth = dot(texture2D(inputTexture, texcoord), bitShift);",
-            "    gl_FragColor = vec4(depth, depth, depth, 1.0);",
+            "    vec2 texCoord = (gl_FragCoord.xy / canvasSize.xy);",
+            "    gl_FragColor = texture2D(inputTexture, texCoord);",
             "}"
         ].join("\n"),
 
@@ -237,4 +200,5 @@
             inputTexture: null
         }
     });
+
 }());
